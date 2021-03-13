4e9bf6cfe0ddf18513b8b2f31834bf5710d985dd
James <50638563+xJamesScott@users.noreply.github.com>
2021-03-13 14:26:00 -0500

    t

--- gotonew.js
+++ gotonew.js
@@ -1,7 +1,6 @@
 gotonew
 newmaster
 gotonew
-<<<<<<< HEAD
 Broadca
 BroadcastCh
 BroadcastCh
@@ -11,6 +10,4 @@ BroadcastCh
 BroadcastCh
 gotonew
 yoooo
-=======
 
->>>>>>> ca93f0f732a26b194a42faa73a2244b656f1afb1
